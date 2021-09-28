d4b2dcb9f6e59824a3f4fdef2464f829220da85d
Zac <zacharyholland@gmail.com>
2021-09-27 18:04:33 -0700

    updated layouts, added css styles

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,27 @@ use rocket_contrib::templates::Template;
 use std::collections::HashMap;
 use std::fs;
 use std::path::PathBuf;
+use walkdir::WalkDir;
 
 const ROOT: &str = env!("NOTES_ROOT");
+
+fn get_tags(root: String) {
+    let mut filenames = HashMap::new();
+
+    for entry in WalkDir::new(root)
+        .into_iter()
+        .filter_map(Result::ok)
+        .filter(|e| !e.file_type().is_dir())
+    {
+        let f_name = String::from(entry.file_name().to_string_lossy());
+        let counter = filenames.entry(f_name.clone()).or_insert(0);
+        *counter += 1;
+
+        if *counter == 2 {
+            println!("{}", f_name);
+        }
+    }
+}
 
 #[get("/<path..>")]
 fn file(path: PathBuf) -> Template {
