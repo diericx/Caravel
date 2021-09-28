b113559584f0d48928784067307f7b4af113f80b
Zac <zacharyholland@gmail.com>
2021-09-27 17:24:47 -0700

    added stylesheet, html template

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,16 @@ extern crate rocket;
 
 use comrak::{markdown_to_html, ComrakOptions};
 use rocket::response::{content, status};
+use rocket_contrib::serve::StaticFiles;
+use rocket_contrib::templates::Template;
+use std::collections::HashMap;
 use std::fs;
 use std::path::PathBuf;
 
-const ROOT: &str = "/home/zac/Nextcloud/notes";
+const ROOT: &str = env!("NOTES_ROOT");
 
 #[get("/<path..>")]
-fn file(path: PathBuf) -> content::Html<String> {
+fn file(path: PathBuf) -> Template {
     let mut full_path = PathBuf::from(ROOT);
     full_path.push(path);
 
@@ -19,9 +22,21 @@ fn file(path: PathBuf) -> content::Html<String> {
         Ok(f) => f,
     };
     let html = markdown_to_html(&data, &ComrakOptions::default());
-    return content::Html(html);
+
+    let mut context = HashMap::new();
+    context.insert("content".to_string(), html.to_string());
+    return Template::render("layout", &context);
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![file]).launch();
+    rocket::ignite();
+
+    rocket::ignite()
+        .attach(Template::fairing())
+        .mount(
+            "/public",
+            StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/static")),
+        )
+        .mount("/notes", routes![file])
+        .launch();
 }
