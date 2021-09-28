8d46494d15df7461e810975dccb5c51f0a787e15
Zac <zacharyholland@gmail.com>
2021-09-28 14:00:53 -0700

    added tag parsing crate

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,22 @@
 #[macro_use]
 extern crate rocket;
 
+mod mdlib;
+
 use comrak::{markdown_to_html, ComrakOptions};
-use rocket::response::{content, status};
+use regex::Regex;
 use rocket_contrib::serve::StaticFiles;
 use rocket_contrib::templates::Template;
 use std::collections::HashMap;
 use std::fs;
+use std::io::BufReader;
+use std::io::{self, BufRead};
 use std::path::PathBuf;
+use walkdir::DirEntry;
 use walkdir::WalkDir;
 
 const ROOT: &str = env!("NOTES_ROOT");
-
-fn get_tags(root: String) {
-    let mut filenames = HashMap::new();
-
-    for entry in WalkDir::new(root)
-        .into_iter()
-        .filter_map(Result::ok)
-        .filter(|e| !e.file_type().is_dir())
-    {
-        let f_name = String::from(entry.file_name().to_string_lossy());
-        let counter = filenames.entry(f_name.clone()).or_insert(0);
-        *counter += 1;
-
-        if *counter == 2 {
-            println!("{}", f_name);
-        }
-    }
-}
+const TAG_CHAR: &str = "#";
 
 #[get("/<path..>")]
 fn file(path: PathBuf) -> Template {
@@ -37,7 +25,7 @@ fn file(path: PathBuf) -> Template {
     full_path.push(path);
 
     let data = match fs::read_to_string(full_path) {
-        Err(e) => String::from("error"),
+        Err(e) => format!("Error: {}", e.to_string()),
         Ok(f) => f,
     };
     let html = markdown_to_html(&data, &ComrakOptions::default());
@@ -50,7 +38,8 @@ fn file(path: PathBuf) -> Template {
 #[get("/")]
 fn tags() -> Template {
     let mut context = HashMap::new();
-    context.insert("tags".to_string(), "".to_string());
+    let tags = mdlib::get_tags_from_dir(ROOT);
+    context.insert(String::from("tags"), tags);
     return Template::render("tags", &context);
 }
 
---
+++ src/mdlib.rs
@@ -0,0 +1,95 @@
+// mdlib is a crate that helps explore a markdown library (read only)
+use crate::TAG_CHAR;
+
+use regex::Regex;
+use std::fs;
+use std::io::BufReader;
+use std::io::{self, BufRead};
+use std::path::PathBuf;
+use walkdir::DirEntry;
+use walkdir::WalkDir;
+
+const MD_EXTENSIONS: [&str; 5] = ["md", "markdown", "mdown", "mkdn", "mkd"];
+
+// Checks if a walkdir::DirEntry is a hidden file
+fn is_file_hidden(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s.starts_with("."))
+        .unwrap_or(false)
+}
+
+// Checks if a walkdir::DirEntry is a markdown file
+fn is_file_markdown(entry: &DirEntry) -> bool {
+    let ext = match entry.path().extension() {
+        Some(ext) => ext,
+        None => return false,
+    };
+    return MD_EXTENSIONS.contains(&ext.to_str().unwrap_or(""));
+}
+
+// Returns a vector of markdown files recursively searched in the provided directory
+fn get_markdown_files_recursive(root: String) -> Vec<DirEntry> {
+    return WalkDir::new(root)
+        .into_iter()
+        .filter_map(Result::ok)
+        .filter(|e| !e.file_type().is_dir())
+        .filter(|e| !is_file_hidden(e))
+        .filter(|e| is_file_markdown(e))
+        .collect();
+}
+
+fn get_tags_from_file(file: DirEntry) -> Vec<String> {
+    let tags: Vec<String> = Vec::new();
+    // Open the file
+    let file = match fs::File::open(file.path()) {
+        Ok(file) => file,
+        Err(_) => panic!("Unable to read title from {}", file.path().display()),
+    };
+    let mut buffer = BufReader::new(file);
+
+    // Get the first line
+    let mut first_line = String::new();
+    match buffer.read_line(&mut first_line) {
+        Ok(n) => n,
+        Err(_) => return Vec::new(),
+    };
+
+    return get_tags_from_line(first_line);
+}
+
+fn get_tags_from_line(line: String) -> Vec<String> {
+    let mut tags: Vec<String> = Vec::new();
+
+    // Opinionated rule, tag line is the first line if and only if it is a simple code line
+    let re = Regex::new(r"^`([A-Za-z0-9# _-]+)`").unwrap();
+    let line_str = line.to_string();
+    let tags_captures = match re.captures(&line_str) {
+        None => return tags,
+        Some(c) => c,
+    };
+    let tags_str = match tags_captures.get(1) {
+        None => return tags,
+        Some(c) => c.as_str(),
+    };
+
+    tags_str
+        .split(" ")
+        .map(|t| t.replace(TAG_CHAR, ""))
+        .for_each(|t| tags.push(t.to_string()));
+
+    return tags;
+}
+
+pub fn get_tags_from_dir(dir: &str) -> Vec<String> {
+    let mut tags: Vec<String> = Vec::new();
+    let root: String = dir.to_string();
+    let files = get_markdown_files_recursive(dir.to_string());
+    for f in files.into_iter() {
+        tags.extend(get_tags_from_file(f));
+    }
+    tags.sort_unstable();
+    tags.dedup();
+    return tags;
+}
